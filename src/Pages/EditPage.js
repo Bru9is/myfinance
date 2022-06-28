66ed2a0143a5eac868223cf26882c20eadcc135a
Ksenia Busquet <ksenia.busquet@gmail.com>
2022-06-27 21:03:23 -0300

    edit page CSS fix

--- src/Pages/EditPage.js
+++ src/Pages/EditPage.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import './EditPage.css'
 
 export default function EditPage() {
     const [title, setTitle] = useState("");
@@ -41,41 +42,63 @@ export default function EditPage() {
  */  }
 
   return (
-    <div>
+    <div className="">
       <h1 className="title">Edit item info</h1>
+      <div className="edit-wrapper">
       <form className="item-creation" onSubmit={handleSubmit}>
+        
+        <div className = 'line-wrapper'>
         <label htmlFor="title">Title</label>
         <input
+          className="edit-input"
           value={info.title}
           name={info.title}
           placeholder="Title"
           onChange={(e) => setInfo({ ...info, title: e.target.value })}
         />
+        </div>
+
+        <div className = 'line-wrapper'>
         <label htmlFor="value">Value</label>
         <input
+          className="edit-input"
           value={info.value}
           name={info.value}
           placeholder="value"
           onChange={(e) => setInfo({ ...info, value: e.target.value })}
         />
+        </div>
+
+
+        <div className = 'line-wrapper date'>
         <label htmlFor="date">Date</label>
         <input
+          className="edit-input"
           type="date"
           value={info.date}
           name={info.date}
           placeholder="date"
           onChange={(e) => setInfo({ ...info, date: e.target.value })}
         />
+        </div>
+
+        <div className = 'line-wrapper'>
         <label htmlFor="category">Category</label>
         <input
+          className="edit-input"
           type="category"
           value={info.category}
           name={info.category}
           placeholder="category"
           onChange={(e) => setInfo({ ...info, category: e.target.value })}
         />
-        <button type="submit">Confirm</button>
+        </div>
+        <button className ='sign-up-button white' type="submit">Confirm</button>
       </form>
+      
+      </div>
+      
+      <Link to = '/homepage' style = {{textAlign: 'center'}}>Back to the homepage</Link>
     </div>
   );
 }
